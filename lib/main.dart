9681094466772f02fe5450c0bad8d09ca39bedda
madani djamel <fd_madani@esi.dz>
2018-12-23 18:12:31 +0100

    Navigate to a new screen

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class RandomWordsState extends State<RandomWords> {
     //Set is preferred to List because a properly implemented Set does not allow 
     //duplicate entries
     final Set<WordPair> _saved = new Set<WordPair>();
-    final TextStyle _biggerFont = const TextStyle(fontSize: 18.0,color: Colors.orangeAccent); 
+    final TextStyle _biggerFont = const TextStyle(fontSize: 18.0,color: Colors.black12); 
 
    Widget _buildSuggestions() {
     return new ListView.builder(
@@ -91,6 +91,38 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
+  void _pushSaved() {
+  Navigator.of(context).push(
+    new MaterialPageRoute<void>(   // Add 20 lines from here...
+      builder: (BuildContext context) {
+        final Iterable<ListTile> tiles = _saved.map(
+          (WordPair pair) {
+            return new ListTile(
+              title: new Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          },
+        );
+        final List<Widget> divided = ListTile
+          .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+          .toList();
+          return new Scaffold(         // Add 6 lines from here...
+          appBar: new AppBar(
+            title: const Text('Saved Suggestions'),
+            backgroundColor: Colors.orangeAccent,
+          ),
+          body: new ListView(children: divided),
+        ); 
+      },
+    ),                           // ... to here.
+  );
+}
+
   @override                                   
   Widget build(BuildContext context) {
     //final WordPair wordPair = new WordPair.random();
@@ -99,6 +131,9 @@ class RandomWordsState extends State<RandomWords> {
       appBar: new AppBar(
         backgroundColor: Colors.orangeAccent,
         title: new Text('Startup Name Generator'),
+         actions: <Widget>[      // Add 3 lines from here...
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+        ], 
       ),
       body: _buildSuggestions(),
     ); 
